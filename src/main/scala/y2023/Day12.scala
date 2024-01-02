d91a0ee3abe06160f5f60ff19df0c0428113ce7d
Nick Sauro <nick.sauro@gmail.com>
2024-01-02 08:02:37 -0500

    day 12 part2 finally

--- src/main/scala/y2023/Day12.scala
+++ src/main/scala/y2023/Day12.scala
@@ -1,66 +1,60 @@
 package y2023
 
-import scala.annotation.tailrec
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
-object Day12 extends App{
+object Day12 extends App {
 
   val data = Source.fromResource("2023/12.data").getLines().toSeq
 
-  /*val res = data.map{ x =>
+  val res = data.map { x =>
     val s = x.split(" ")
     val springs = s.head.trim
     val counts = s.last.split(",").map(_.trim.toInt)
     val n = springs.count(_ == '?')
     val possibilities = Math.pow(2, n)
-    val r =  0 until possibilities.toInt
-    //println(s"parsing: ${s.head} -- ${counts.mkString(",")} -- ${n}")
-    val bla = r.count{ p =>
-      val s =  p.toBinaryString
+    val r = 0 until possibilities.toInt
+    val res = r.count { p =>
+      val s = p.toBinaryString
       val padding = new String(Array.fill(n - s.length)('0'))
       val opt = merge(springs.toArray, s.prependedAll(padding))
-    //  println(opt.mkString(""))
-      val m = matches(opt, counts)
-     // println(s"matches: $m")
-      m
+      matches(opt, counts)
     }
-    println(s"matches: $bla")
-    bla
-  }.sum
-
-
-  println(res)*/
-
-  /*val res2 = data.map{ x =>
-    val s = x.split(" ")
-    val springs = s.head.trim
-    val expandedSprings = springs  ///Seq.fill(5)(springs).mkString("?")
-    val expandedCounts = s.last.trim /// Seq.fill(5)(s.last.trim).mkString(",")
-    println(expandedSprings)
-    println(expandedCounts)
-    val qLocs = expandedSprings.zipWithIndex.collect{
-      case ('?', i) => i
-    }
-    val res = dfs(expandedSprings.toArray, qLocs, expandedCounts.split(",").map(_.trim.toInt), 0, 0)
-    println(res)
+   // println(s"$springs -- $res")
     res
   }.sum
-  println(res2)*/
 
-  def matches(s : Array[Char], groups : Seq[Int]) : Boolean = {
-    println(s"${s.mkString("")} -- ${groups.mkString(",")}" )
+
+  println(res)
+  val cache = mutable.HashMap.empty[(String, Seq[Int]), Long]
+
+  val expanded = data.map{ x =>
+    val s = x.split(" ")
+    val springs = s.head.trim
+    val counts = s.last.split(",").map(_.trim.toInt)
+    val expandedSprings = Seq.fill(5)(springs).mkString("?")
+    val expandedCounts = counts ++ counts ++ counts ++ counts ++ counts
+    val res = part2(expandedSprings, expandedCounts)
+    //println(s"$x - $res")
+    //cache.toSeq.sortBy(_._1._1.length).foreach(println)
+    res
+
+  }.sum
+
+  println(expanded)
+
+  def matches(s: Array[Char], groups: Seq[Int]): Boolean = {
     val l = ListBuffer.empty[Int]
-    for{
+    for {
       i <- s.indices
     } {
-      if(i == 0){
-        if(s(i) == '#')  l.addOne(1)
-      }else{
+      if (i == 0) {
+        if (s(i) == '#') l.addOne(1)
+      } else {
         val prev = s(i - 1)
         val cur = s(i)
-        (prev,cur) match {
+        (prev, cur) match {
           case ('#', '#') => l(l.size - 1) = l.last + 1
           case ('.', '.') =>
           case ('.', '#') => l.addOne(1)
@@ -72,71 +66,53 @@ object Day12 extends App{
 
   }
 
-  def merge(s : Array[Char], subs : String) = {
+  def merge(s: Array[Char], subs: String) = {
     val q = mutable.Queue.from(subs)
-    s.mapInPlace{
+    s.mapInPlace {
       case '?' => {
         val next = q.dequeue()
-        next match{
+        next match {
           case '1' => '#'
           case _ => '.'
         }
       }
       case o => o
     }
+  }
+
+
+
+  def part2(s : String, groups: Seq[Int]) : Long = {
+    val k = (s, groups)
+    if(cache.contains(k)){
+      cache(k)
+    }else{
+      val res =
+        if(s.isEmpty){ //remaining groups..no chars
+        if(groups.isEmpty) 1 else 0
+      }else if(groups.isEmpty){  ///fully exhausted
+        if(!s.contains('#')) 1 else 0
+      }else{
+        s.head match{
+          case '#'  if s.length >= groups.head => {
+            val (m, remaining) = s.splitAt(groups.head)
+            if(m.exists(_ == '.') || (remaining.nonEmpty && remaining.head == '#')){
+              0
+            }else{
+              part2(remaining.tail, groups.tail)
+            }
+          }
+          case '#' => 0
+          case '?' => {
+            part2("." + s.tail, groups) + part2("#" + s.tail, groups)
+          }
+          case '.' => part2(s.tail, groups)
+        }
+      }
+      cache.addOne(k, res)
+      res
+    }
 
   }
 
-  /*def dfs(s: Array[Char], qLocs:Seq[Int], group: Array[Int], q: Int, acc: Int) : Int = {
-    s(qLocs(q)) = '.'
-    val qres = check2(s, group, 0,0) match{
-      case Valid => 1
-      case Invalid => 0
-      case Inconclusive => dfs(s, qLocs, group, q + 1, acc)
-    }
-    s(qLocs(q)) = '#'
-    val pres = check2(s, group, 0,0) match {
-      case Valid => 1
-      case Invalid => 0
-      case Inconclusive => dfs(s, qLocs, group, q + 1, acc)
-    }
-    pres + acc + qres
-  }*/
-
-
-
-  /*def check2(s: Array[Char], groups: Array[Int], curG: Int, visitedPs: mutable.HashSet[Int]): Validity = {
-    println(s"${s.mkString("")} -- ${groups.mkString(",")} -- $visitedPs -- $curG")
-
-    if(!s.contains('?')){
-      val r = matches(s, groups)
-      if(r){
-        println("valid")
-        Valid
-      }else{
-        println("invalid")
-        Invalid
-      }
-    }else{
-     // s.
-      //just need to determine invalid
-      val firstQ = s.indexOf('?')
-      val firstP = s.indexWhere( x => x == '#')
-      if(firstQ < firstP){
-        Inconclusive
-      }else{
-
-      }
-
-
-      }
-    }
-  }*/
 }
-
-
-sealed trait Validity
-
-case object Valid extends Validity
-case object Invalid extends Validity
-case object Inconclusive extends Validity
